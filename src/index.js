b01ff6ad076eb2a53736a20bdb317825da0e12e6
nikolassantos4444 <nikolassantos4444@gmail.com>
2025-12-02 12:08:27 +0000

    inicio e conclusao do backend

---
+++ src/index.js
@@ -0,0 +1,83 @@
+import express from "express"
+import cors from "cors"
+import mysql from "mysql2"
+
+const { DB_HOST, DB_USER, DB_NAME, DB_PASSWORD } = process.env
+
+const app = express()
+const port = 3333
+
+app.use(cors())
+app.use(express.json())
+app.get("/", (request, response) => {
+    const searchCommand = "SELECT id, name, email, nickname FROM ronnybezerra_02tb"
+
+    database.query(searchCommand, (error, users) => {
+        if (error) {
+            console.log(error)
+            return
+        }
+
+        response.json(users)
+    })
+})
+
+app.post("/login", (request, response) => {
+    const {email, password} = request.body.user
+
+    const selectCommand = `
+        SELECT *
+        FROM marciomarcal_02tb
+        WHERE email = ?
+    `
+
+    database.query(selectCommand, [email], (error, user) => {
+        if (error) {
+            console.log(error)
+            return
+        }
+
+        if (user.length === 0 || user[0].password  !== password) {
+            response.json({ message: "Usuário ou senha incorretos:" })
+            return
+        }
+
+        response.json({ 
+            id: user[0].id,
+            name: user[0].name
+        })
+    })
+})
+
+app.post("/cadastrar", (request, response) => {
+    const { user } = request.body
+
+    console.log(user)
+
+    const insertCommand = `
+        INSERT INTO ronnybezerra_02tb(name, email, password, nickname)
+        VALUES (?, ?, ?, ?)
+    
+    `
+
+    database.query(insertCommand, [user.name, user.email, user.password, user.nickname], (error) => {
+        if(error) {
+            console.log(error)
+            return
+        }
+    })
+
+    response.status(281).json({ message: "Usuário cadastrado com sucesso!" })
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta: ${port}!`)
+})
+
+const database = mysql.createPool({
+    host: DB_HOST,
+    database: DB_NAME,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    connectionLimit: 10
+})
